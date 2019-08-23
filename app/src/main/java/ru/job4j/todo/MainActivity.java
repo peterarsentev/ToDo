7ffb1399847b65ce32b2deec47fce84abcb4d61a
Petr Arsentev <parsentev@yandex.ru>
2019-08-23 13:25:14 +0300

    Урок 4.	RecyclerView, Android.

--- app/src/main/java/ru/job4j/todo/MainActivity.java
+++ app/src/main/java/ru/job4j/todo/MainActivity.java
@@ -1,13 +1,67 @@
 package ru.job4j.todo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final List<Item> items = new ArrayList<>();
+    private final RecyclerView.Adapter adapter = new ItemAdapter(this.items);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recycler = findViewById(R.id.recycler);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        recycler.setAdapter(adapter);
+    }
+
+    public void add(View view) {
+        EditText edit = this.findViewById(R.id.editText);
+        this.items.add(new Item(edit.getText().toString()));
+        edit.setText("");
+        adapter.notifyItemInserted(this.items.size() - 1);
+    }
+
+    private static final class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private final List<Item> items;
+
+        public ItemAdapter(List<Item> items) {
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int index) {
+            return new RecyclerView.ViewHolder(
+                    LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.items, parent, false)
+            ) {};
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int index) {
+            TextView name = holder.itemView.findViewById(R.id.name);
+            name.setText(String.format("%s. %s", index, this.items.get(index).getName()));
+        }
+
+        @Override
+        public int getItemCount() {
+            return this.items.size();
+        }
     }
 }
