f4195090b3d546d8c6517a1265901694b942895a
Petr Arsentev <parsentev@yandex.ru>
2019-10-03 16:10:56 +0300

    Урок 8. Фрагменты.

--- app/src/main/java/ru/job4j/todo/AddItemFrg.java
+++ app/src/main/java/ru/job4j/todo/AddItemFrg.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +12,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.Calendar;
-import java.util.Date;
-
-import static ru.job4j.todo.DateFrm.format;
 
 public class AddItemFrg extends Fragment {
-    private Button deadline;
 
     @Nullable
     @Override
@@ -27,25 +21,7 @@ public class AddItemFrg extends Fragment {
         View view = inflater.inflate(R.layout.add, container, false);
         Button saved = view.findViewById(R.id.add);
         saved.setOnClickListener(btn -> save(view));
-        deadline = view.findViewById(R.id.date);
-        FragmentManager fm = getFragmentManager();
-        deadline.setOnClickListener(
-                btn -> {
-                    DatePickerFrg dialog = new DatePickerFrg();
-                    dialog.setTargetFragment(AddItemFrg.this, 0);
-                    dialog.show(fm, "date");
-                }
-        );
         return view;
-    }
-
-    @Override
-    public void onActivityResult(int request, int result, Intent data) {
-        if (result == 0) {
-            Calendar cal = Calendar.getInstance();
-            cal.setTimeInMillis(data.getLongExtra("deadline", 0));
-            deadline.setText(format(cal));
-        }
     }
 
     public void save(View view) {
